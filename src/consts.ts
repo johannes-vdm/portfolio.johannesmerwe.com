fa3f2992ffc3559ab041667e4c54f237fccebf26
Johannes van der Merwe <Johannesmerwede@gmail.com>
2023-12-25 18:13:01 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -4,16 +4,6 @@ export const SITE = {
 	defaultLanguage: 'en-us',
 } as const;
 
-// export const OPEN_GRAPH = {
-// 	// image: {
-// 		// src: 'https://github.com/withastro/astro/blob/main/.github/assets/banner.png?raw=true',
-// 	// 	alt:
-// 	// 		'astro logo on a starry expanse of space,' +
-// 	// 		' with a purple saturn-like planet floating in the right foreground',
-// 	// },
-// 	// twitter: 'astrodotbuild',
-// };
-
 export const KNOWN_LANGUAGES = {
 	English: 'en',
 } as const;

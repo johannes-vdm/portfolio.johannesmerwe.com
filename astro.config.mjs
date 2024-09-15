7479ad4d251f890f5d2f3d0179bf94fc6d84b1d7
Johannes van der Merwe <Johannesmerwede@gmail.com>
2024-09-15 11:36:37 +0000

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import react from '@astrojs/react';
 
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(), sitemap()],
-	site: `https://portfolio.johannesmerwe.com`
+    integrations: [// Enable Preact to support Preact JSX components.
+    preact(), // Enable React for the Algolia search component.
+    react(), sitemap(), tailwind()],
+    site: `https://portfolio.johannesmerwe.com`
 });

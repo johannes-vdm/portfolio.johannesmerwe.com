39d09ae1e048a19f581d6a4895ca3c01ff3328e4
Johannes van der Merwe <Johannesmerwede@gmail.com>
2024-09-15 09:59:04 +0000

    new structure woop woop

--- src/consts.ts
+++ src/consts.ts
@@ -1,45 +1,23 @@
 export const SITE = {
 	title: 'Johannes van der Merwe',
 	description: 'Johannes van der Merwe',
-	defaultLanguage: 'en-us',
 } as const;
 
-export const KNOWN_LANGUAGES = {
-	English: 'en',
-} as const;
-export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
-
-
-// See "Algolia" section of the README for more information.
-export const ALGOLIA = {
-	indexName: 'XXXXXXXXXX',
-	appId: 'XXXXXXXXXX',
-	apiKey: 'XXXXXXXXXX',
-};
-
-export type Sidebar = Record<
-	(typeof KNOWN_LANGUAGE_CODES)[number],
-	Record<string, { text: string; link: string }[]>
->;
+export type Sidebar = Record<string, { text: string; link: string }[]>;
 export const SIDEBAR: Sidebar = {
-	en: {
-		// 'Home': [
-		// 	{ text: 'Home', link: 'en/home' },
-		// ],
-		'Careman': [
-			{ text: 'Front-End', link: 'en/careman-fontend' },
-			{ text: 'Introduction', link: 'en/intro' },
-			{ text: 'Tree', link: 'en/tree' },
-			{ text: 'DB', link: 'en/database' },
-			{ text: 'API', link: 'en/api' },
-			{ text: 'SMS', link: 'en/sms' },
-			{ text: 'Commands', link: 'en/commands' },
-		],
-		'GPT Watchtower': [
-			{ text: 'Introduction', link: 'en/gpt-intro' },
-			{ text: 'Tree', link: 'en/tree-watchtower' },
-			{ text: 'Watchtower', link: 'en/watchtower' },
-			{ text: 'Express Beacon', link: 'en/express-beacon' },
-		]
-	},
+	'Careman': [
+		{ text: 'Introduction', link: 'careman' },
+		{ text: 'Interface', link: 'careman/careman-fontend' },
+		{ text: 'Tree', link: 'careman/tree' },
+		{ text: 'DB', link: 'careman/database' },
+		{ text: 'API', link: 'careman/api' },
+		{ text: 'SMS', link: 'careman/sms' },
+		{ text: 'Commands', link: 'careman/commands' },
+	],
+	'GPT Screenshot': [
+		{ text: 'Introduction', link: 'gpt-screenshot' },
+		{ text: 'Tree', link: 'gpt-screenshot/tree-watchtower' },
+		{ text: 'Watchtower', link: 'gpt-screenshot/watchtower' },
+		{ text: 'Express Beacon', link: 'gpt-screenshot/express-beacon' },
+	]
 };
